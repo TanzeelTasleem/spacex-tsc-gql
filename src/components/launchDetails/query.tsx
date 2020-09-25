a3537888af6a21799306c2ee7868bf35f6fafebc
Tanzeel Tasleem <53001056+TanzeelTasleem@users.noreply.github.com>
2020-09-26 00:22:41 +0500

    args to graphql provided

---
+++ src/components/launchDetails/query.tsx
@@ -0,0 +1,30 @@
+import { gql } from "@apollo/client";
+
+export const Query_LaunchDetails = gql`
+  query launchDetails($id: ID!) {
+    launch(id: $id) {
+      mission_name
+      details
+      id
+      launch_year
+      launch_success
+      links {
+        flickr_images
+        video_link
+        article_link
+      }
+      rocket {
+        rocket_name
+        rocket_type
+      }
+      upcoming
+      ships {
+        name
+      }
+      launch_site {
+        site_name
+        site_name_long
+      }
+    }
+  }
+`;
